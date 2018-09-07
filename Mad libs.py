5b58841cb8e4316a0a04b34efe4daa5a87e9a7cd
Kyle Baldwin <kyle.baldwin@tooeleschools.org>
2018-09-07 13:25:51 -0600

    first commit, 4/5 Madlibs

---
+++ Mad libs.py
@@ -0,0 +1,22 @@
+color1 = input("color")
+color2 = input(" color ")
+adjective1 = input(' adjective ')
+animals1 = input(" aniaml ")
+animals2 = input(" animal ")
+animals3 = input(' animal ')
+color3 = input(" color ")
+noun1 = input(" noun ")
+verb_ing1 = input(" verb-ing ")
+color4 = input(" color ")
+adjective2 = input(" adjective ")
+color5 = input(" color ")
+verb1 = input(" verb ")
+adjective3 = input(" smelly ")
+
+print(" Spring is a season with lots of rain and " + color1 + " and " + color2 +
+      " flowers. " + adjective1 + animals1 + " chirp in the treesand many " + adjective2 + " baby " + animals2
+      + " and " + animals3 + " are born. Before it rains, the sky turns "
+      + color3 + " and " + noun1 + " starts " + verb_ing1 + " from the sky. " + adjective3 + color4
+      + " rain boots and " + verb1 + " in puddles. If you're " + adjective3 + " you might see a rainbow ")
+
+input
